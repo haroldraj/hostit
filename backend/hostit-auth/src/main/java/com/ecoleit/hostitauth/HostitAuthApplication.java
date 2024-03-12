914d5fb8362ab44748f68f102590a0c8e56de4d7
Harold <rnarsonharold@gmail.com>
2024-03-12 22:19:13 +0100

    first commit

---
+++ backend/hostit-auth/src/main/java/com/ecoleit/hostitauth/HostitAuthApplication.java
@@ -0,0 +1,13 @@
+package com.ecoleit.hostitauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HostitAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HostitAuthApplication.class, args);
+	}
+
+}
---
+++ backend/hostit-auth/src/test/java/com/ecoleit/hostitauth/HostitAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecoleit.hostitauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HostitAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
