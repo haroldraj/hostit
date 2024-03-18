fcc1b4fe0c45f8950f5550cc50412fe0af71cdd4
takkamdarios <takkamdarios@gmail.com>
2024-03-18 19:36:11 +0100

    Code readjusted

--- backend/hostit-auth/src/main/java/com/ecoleit/hostitauth/service/UserService.java
+++ backend/hostit-auth/src/main/java/com/ecoleit/hostitauth/service/UserService.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class UserService {
 
@@ -29,6 +31,15 @@ public class UserService {
 
     private boolean emailExist(String email) {
         return userRepository.findUserByEmail(email).isPresent();
+    }
+
+
+    public Optional<User> findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
     }
 
     // Additional methods like validateUser, deleteUser, etc., could also be here.
