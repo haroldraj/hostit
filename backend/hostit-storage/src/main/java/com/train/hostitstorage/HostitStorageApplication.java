c217eba53a7a05c1870dd9ccf9d5531d52c485fd
franckessi <essitsondofranckpatrick@gmail.com>
2024-05-13 21:16:08 +0200

    Configuring minio for storage

--- backend/hostit-storage/src/main/java/com/train/hostitstorage/HostitStorageApplication.java
+++ backend/hostit-storage/src/main/java/com/train/hostitstorage/HostitStorageApplication.java
@@ -1,5 +1,6 @@
 package com.train.hostitstorage;
 
+import io.minio.MinioClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HostitStorageApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HostitStorageApplication.class, args);
+		//SpringApplication.run(HostitStorageApplication.class, args);
+		MinioClient minioClient = MinioClient.builder()
+				.endpoint("https://play.min.io")
+				.credentials("B7CvxeruN5dwA9oGJEGr", "dOzJqkAh80L6veSwog1jKUqm79tWcN152qBoB5Ez")
+				.build();
 	}
+
 
 }
