a21db1cd668749a4769ffdc2563052347afcbcbf
Harold <rnarsonharold@gmail.com>
2024-05-13 21:50:43 +0200

    hostit-analytics project creation and first commit

---
+++ backend/hostit-analytics/src/test/java/com/ecoleit/hostitanalytics/HostitAnalyticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecoleit.hostitanalytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HostitAnalyticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
