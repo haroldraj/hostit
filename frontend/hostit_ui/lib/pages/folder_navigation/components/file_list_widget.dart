d6f7df00e6bf759113e6994ed4213cf5265fb947
Harold <rnarsonharold@gmail.com>
2024-05-31 02:44:10 +0200

    refactor

--- frontend/hostit_ui/lib/pages/folder_navigation/components/file_list_widget.dart
+++ frontend/hostit_ui/lib/pages/folder_navigation/components/file_list_widget.dart
@@ -1,112 +1,4 @@
-/*import 'package:flutter/material.dart';
-import 'package:hostit_ui/constants/custom_colors.dart';
-import 'package:hostit_ui/constants/helpers.dart';
-import 'package:hostit_ui/models/folder_content_model.dart';
-import 'package:hostit_ui/providers/folder_path_provider.dart';
-import 'package:hostit_ui/responsive.dart';
-import 'package:hostit_ui/widgets/custom_data_table.dart';
-import 'package:provider/provider.dart';
-
-class FileListWidget extends StatefulWidget {
-  final List<FolderContentModel>? files;
-  const FileListWidget({super.key, required this.files});
-
-  @override
-  State<FileListWidget> createState() => _FileListWidgetState();
-}
-
-class _FileListWidgetState extends State<FileListWidget> {
-  final TextEditingController _searchController = TextEditingController();
-
-  @override
-  Widget build(BuildContext context) {
-    return Consumer<FolderPathProvider>(
-      builder: (context, folderPathProvider, child) {
-        var folderPath = folderPathProvider.folderPath;
-        return Column(
-          children: [
-            Container(
-              margin: const EdgeInsets.only(bottom: 25, left: 15),
-              child: Row(
-                children: folderPath.asMap().entries.expand((entry) {
-                  int idx = entry.key;
-                  String folder = entry.value;
-
-                  return [
-                    TextButton(
-                      onPressed: () {
-                        folderPathProvider
-                            .setFolderPath(folderPath.sublist(0, idx + 1));
-                      },
-                      child: Text(
-                        folder,
-                        style: const TextStyle(fontSize: 20),
-                      ),
-                    ),
-                    if (idx < folderPath.length - 1)
-                      const Text(
-                        ' > ',
-                        style: TextStyle(fontSize: 40),
-                      ),
-                  ];
-                }).toList(),
-              ),
-            ),
-            Container(
-              margin: const EdgeInsets.symmetric(horizontal: 10),
-              child: TextField(
-                decoration: InputDecoration(
-                  hintText: "Search Files",
-                  fillColor: CustomColors.searchFeldColor,
-                  filled: true,
-                  border: const OutlineInputBorder(
-                    borderSide: BorderSide.none,
-                    borderRadius: BorderRadius.all(Radius.circular(25)),
-                  ),
-                  prefixIcon: const Icon(Icons.search),
-                ),
-                controller: _searchController,
-                onChanged: (value) {
-                  setState(() {});
-                },
-              ),
-            ),
-            Spacing.vertical,
-            CustomDataTable(
-              folderNavigation: true,
-              fullScreen: true,
-              clickable: false,
-              showActionsColumn: true,
-              columns: Responsive.isMobile(context)
-                  ? const ["Name"]
-                  : const ["Name", "Size", "Last modified"],
-              data: widget.files
-                      ?.where((file) => file.name!
-                          .toLowerCase()
-                          .contains(_searchController.text.toLowerCase()))
-                      .map(
-                        (file) => Responsive.isMobile(context)
-                            ? [file.name]
-                            : [
-                                file.name,
-                                file.sizeToString,
-                                file.type,
-                              ],
-                      )
-                      .toList() ??
-                  [],
-              context: context,
-            ),
-          ],
-        );
-      },
-    );
-  }
-}
-*/
-
 import 'package:flutter/material.dart';
-import 'package:hostit_ui/constants/helpers.dart';
 import 'package:hostit_ui/models/folder_content_model.dart';
 import 'package:hostit_ui/providers/folder_path_provider.dart';
 import 'package:hostit_ui/responsive.dart';
@@ -117,7 +9,7 @@ import 'package:hostit_ui/widgets/custom_progress_indicator.dart';
 import 'package:provider/provider.dart';
 
 class FileListWidget extends StatefulWidget {
-  const FileListWidget({Key? key}) : super(key: key);
+  const FileListWidget({super.key});
 
   @override
   State<FileListWidget> createState() => _FileListWidgetState();
--- frontend/hostit_ui/lib/pages/folder_navigation/folder_navigation_page.dart
+++ frontend/hostit_ui/lib/pages/folder_navigation/folder_navigation_page.dart
@@ -1,76 +1,3 @@
-/*import 'package:flutter/material.dart';
-import 'package:hostit_ui/constants/custom_colors.dart';
-import 'package:hostit_ui/constants/helpers.dart';
-import 'package:hostit_ui/models/folder_content_model.dart';
-import 'package:hostit_ui/pages/folder_navigation/components/file_list_widget.dart';
-import 'package:hostit_ui/providers/folder_path_provider.dart';
-import 'package:hostit_ui/responsive.dart';
-import 'package:hostit_ui/service/folder_service.dart';
-import 'package:hostit_ui/service/user_service.dart';
-import 'package:hostit_ui/widgets/custom_data_table.dart';
-import 'package:hostit_ui/widgets/custom_progress_indicator.dart';
-import 'package:provider/provider.dart';
-
-class FolderNavigationPage extends StatefulWidget {
-  const FolderNavigationPage({super.key});
-
-  @override
-  State<FolderNavigationPage> createState() => _FolderNavigationPageState();
-}
-
-class _FolderNavigationPageState extends State<FolderNavigationPage> {
-  final FolderService _folderService = FolderService();
-  int userId = UserService().getUserId();
-
-  @override
-  Widget build(BuildContext context) {
-    return Expanded(
-      child: Container(
-        decoration: BoxDecoration(
-          color: CustomColors.pageBgColor,
-          borderRadius: BorderRadius.circular(20),
-        ),
-        child: Consumer<FolderPathProvider>(
-          builder: (context, folderPathProvider, child) {
-            return FutureBuilder<List<FolderContentModel>>(
-              future: _folderService.getFolderContent(
-                  userId,
-                  folderPathProvider.folderPath.isEmpty
-                      ? ".root"
-                      : folderPathProvider.folderPathToString),
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return Center(
-                    child:
-                        customCircularProgressIndicator("Loading file list..."),
-                  );
-                } else if (snapshot.hasData && snapshot.data!.isNotEmpty) {
-                  var files = snapshot.data;
-                  return SingleChildScrollView(
-                    scrollDirection: Axis.vertical,
-                    child: Column(
-                      children: [
-                        Spacing.vertical,
-                        FileListWidget(files: files),
-                      ],
-                    ),
-                  );
-                } else if (snapshot.hasError) {
-                  return Center(
-                    child: Text("Error: ${snapshot.error}"),
-                  );
-                } else {
-                  return const Center(child: Text("No files found"));
-                }
-              },
-            );
-          },
-        ),
-      ),
-    );
-  }
-}*/
-
 import 'package:flutter/material.dart';
 import 'package:hostit_ui/constants/custom_colors.dart';
 import 'package:hostit_ui/constants/helpers.dart';
@@ -79,6 +6,8 @@ import 'package:hostit_ui/providers/folder_path_provider.dart';
 import 'package:provider/provider.dart';
 
 class FolderNavigationPage extends StatelessWidget {
+  const FolderNavigationPage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Expanded(
@@ -87,14 +16,14 @@ class FolderNavigationPage extends StatelessWidget {
           color: CustomColors.pageBgColor,
           borderRadius: BorderRadius.circular(20),
         ),
-        child: Column(
+        child: const Column(
           children: [
             Spacing.vertical,
-            const FolderPathWidget(),
+            FolderPathWidget(),
             Spacing.vertical,
-            const SearchFieldWidget(),
+            SearchFieldWidget(),
             Spacing.vertical,
-            const Expanded(child: FileListWidget()),
+            Expanded(child: FileListWidget()),
           ],
         ),
       ),
@@ -103,7 +32,7 @@ class FolderNavigationPage extends StatelessWidget {
 }
 
 class FolderPathWidget extends StatelessWidget {
-  const FolderPathWidget({Key? key}) : super(key: key);
+  const FolderPathWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -143,7 +72,7 @@ class FolderPathWidget extends StatelessWidget {
 }
 
 class SearchFieldWidget extends StatelessWidget {
-  const SearchFieldWidget({Key? key}) : super(key: key);
+  const SearchFieldWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
