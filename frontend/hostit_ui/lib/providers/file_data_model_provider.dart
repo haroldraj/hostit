8f15748a71c8393bf3447cb2153099a68c14e855
Harold <rnarsonharold@gmail.com>
2024-06-01 09:01:39 +0200

    .

--- frontend/hostit_ui/lib/providers/file_data_model_provider.dart
+++ frontend/hostit_ui/lib/providers/file_data_model_provider.dart
@@ -13,24 +13,6 @@ class FileDataModelProvider extends ChangeNotifier {
   final Logger _logger = Logger();
   List<FileModel> get files => _files;
 
-  Future<List<FileModel>>? getUserFiles() async {
-    int userId = UserService().getUserId();
-    final url = Uri.parse("$_baseUrl/files?userId=$userId");
-    final response = await http.get(url, headers: ngrokHeaders);
-    if (response.statusCode == 200) {
-      _logger.i("Data fetched");
-      final List<dynamic> userFiles = json
-          .decode(utf8.decode(response.bodyBytes))["result"] as List<dynamic>;
-      _logger.i(userFiles);
-      var mappedFiles =
-          userFiles.map((file) => FileModel.fromJson(file)).toList();
-      _logger.i(mappedFiles);
-      return mappedFiles;
-    } else {
-      throw Exception('Failed on fetching the list of files');
-    }
-  }
-
   Future<void> fetchFiles() async {
     int userId = UserService().getUserId();
     final url = Uri.parse("$_baseUrl/files?userId=$userId");
@@ -45,28 +27,4 @@ class FileDataModelProvider extends ChangeNotifier {
       throw Exception('Failed to load files');
     }
   }
-
-  /* Future<void> addFile(FileModel file) async {
-    final response = await http.post(
-      Uri.parse('https://your-backend-url.com/files'),
-      headers: {'Content-Type': 'application/json'},
-      body: json.encode(file.toJson()),
-    );
-    if (response.statusCode == 201) {
-      fetchFiles();
-    } else {
-      throw Exception('Failed to add file');
-    }
-  }
-
-  Future<void> deleteFile(int fileId) async {
-    final response = await http.delete(
-      Uri.parse('https://your-backend-url.com/files/$fileId'),
-    );
-    if (response.statusCode == 200) {
-      fetchFiles();
-    } else {
-      throw Exception('Failed to delete file');
-    }
-  }*/
 }
